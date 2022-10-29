9c58987b0424db45d237a159ec2acd50b9246cfd
psm7177 <raspberry-pi@kakao.com>
2022-10-29 16:51:23 +0900

    Add ATM, Bank, Card, Language, Transaction class

---
+++ VodkaATM/ATM.cpp
@@ -0,0 +1,35 @@
+#include "Language.h"
+#include "Transaction.h"
+#include "Card.h"
+#include <stdlib.h>
+#include <time.h>
+#include <list>
+
+class ATM {
+private:
+	int serialNumber;
+	bool isMulti;
+	bool bilingual;
+	string primaryBank;
+
+	Language* descriptor;
+	list<Transaction> transactions;
+	Card* insertedCard;
+
+	int sessionCount;
+	int cash;
+public:
+	ATM(int serial, bool isMulti, bool bilingual,string primaryBank,int initialMoney) {
+		this->serialNumber = serial;
+		this->isMulti = isMulti;
+		this->bilingual = bilingual;
+		this->primaryBank = primaryBank;
+
+		this->descriptor = nullptr;
+
+		this->transactions = list<Transaction>();
+		this->insertedCard = nullptr;
+		this->sessionCount = 0;
+		this->cash = initialMoney;
+	}
+};
