addcb037e65e81967c0c6296b0e8c319826a1af8
psm7177 <raspberry-pi@kakao.com>
2022-11-26 21:38:15 +0900

    Implement onoff

--- VodkaATM/Language.cpp
+++ VodkaATM/Language.cpp
@@ -1,8 +1,22 @@
 #pragma once
 #include "Language.h"
 
+bool Language::onoff = false;
+
 string Language::Eng2Kor(string s) {
+	if (!onoff) {
+		return s;
+	}
 	if (s == "Welcome\n\nTo start, please insert your debit card") {
 		return "반갑습니다\n\n거래를 시작하시려면 카드를 넣어주세요";
+	}
+	if (s == "Transaction ID: ") {
+		return "거래 아이디: ";
+	}
+	if (s == " occured\n") {
+		return " \n";
+	}
+	if (s == "Transaction ID: ") {
+		return "거래 아이디: ";
 	}
 }
